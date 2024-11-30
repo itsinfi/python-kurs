eab031f8fdaed4950975a6f21f7617d2db89b026
itsinfi <sascha.angermann2002@gmail.com>
2024-11-30 16:42:06 +0100

    ha1

--- 01/README.md
+++
@@ -1,3 +0,0 @@
-1. `conda env create -f environment.yml`
-2. `conda activate 01_saschaangermann`
-3. python 01\py.py
--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ print(hello_world)
 print(hello_world.lower())
 print(hello_world.upper())
 print(hello_world.swapcase())
+
